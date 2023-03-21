6b98a3d12f2a599596d4cabce7fefb91e0a2df60
keebot-png <kee.bot@icloud.com>
2023-03-21 13:23:01 +0200

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     # before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    user_categories_path(current_user.id)
+    categories_path(current_user.id)
   end
 
   protected
