1a20632cf3e07e3f91a7603c53aefd04b44cf364
keebot-png <kee.bot@icloud.com>
2023-03-23 20:47:16 +0200

    Add active storage and image processing gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :payments
   belongs_to :author, class_name: 'User'
+  has_one_attached :icon
 
   validates :name, presence: true, length: { maximum: 250 }
   # validates :icon, presence: true
